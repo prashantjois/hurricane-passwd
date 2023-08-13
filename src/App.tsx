279ae459855b30227f40ca5d54c6e2013b208aa8
Prashant Jois <github@jois.ca>
2023-08-13 16:15:06 -0700

    set document title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  useEffect(() => {
+    document.title = 'Hurricane Passwords';
+  }, []); // The empty dependency array ensures this runs once when the component mounts.
+
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
